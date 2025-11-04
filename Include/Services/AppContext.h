1c2799acf7de99bfb260bf1aee39467677a97d8b
Justin Kuchmy <Jukuchmy@gmail.com>
2025-11-04 12:09:44 +0800

    added more repos and services

--- Include/Services/AppContext.h
+++ Include/Services/AppContext.h
@@ -2,7 +2,11 @@
 #define APPCONTEXT_H
 #include <memory>
 #include "Services/EmployeeService.h"
+#include "Services/DependentService.h"
+#include "Services/EmergencyContactService.h"
 #include "Repositories/EmployeeRepository.h"
+#include "Repositories/DependentRepository.h"
+#include "Repositories/EmergencyContactRepository.h"
 #include <sqlite3.h>
 
 class AppContext 
@@ -11,11 +15,19 @@ public:
     static AppContext& instance(const std::string& dbName = "payroll.db");
   
     EmployeeRepository& employeeRepo();
+    DependentRepository& dependentRepo();
+    EmergencyContactRepository& emergencyContactRepo();
     EmployeeService& employeeService();
+    DependentService& dependentService();
+    EmergencyContactService& emergencyContactService();
 private: 
     sqlite3* m_db = nullptr;
     std::unique_ptr<EmployeeRepository> m_employeeRepo;
+    std::unique_ptr<DependentRepository> m_dependentRepo;
+    std::unique_ptr<EmergencyContactRepository> m_emergencyContactRepo;
     std::unique_ptr<EmployeeService> m_employeeService;
+    std::unique_ptr<DependentService> m_dependentService;
+    std::unique_ptr<EmergencyContactService> m_emergencyContactService;
 
 
     explicit AppContext(const std::string& dbName);
