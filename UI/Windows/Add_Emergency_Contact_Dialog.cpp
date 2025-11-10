b9c0d81738dd3df8e24d3c6d700e9aaab110f0e0
Justin Kuchmy <Jukuchmy@gmail.com>
2025-11-10 15:14:57 +0800

    //disable auto close after accepted

--- UI/Windows/Add_Emergency_Contact_Dialog.cpp
+++ UI/Windows/Add_Emergency_Contact_Dialog.cpp
@@ -1,9 +1,11 @@
 #include "include/Generated/ui_add_emergency_contact.h"
 #include "UI/Add_Emergency_Contact_Dialog.h"
+#include "Services/AppContext.h"
 
 EmergencyContactDialog::EmergencyContactDialog(QWidget *parent): QDialog(parent), ui(new Ui::EmergencyContactDialog)
 {
     ui->setupUi(this); 
+    disconnect(ui->buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept); //disable auto close after accepted
     connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &EmergencyContactDialog::onOKClicked);
     connect(ui->buttonBox, &QDialogButtonBox::rejected, this, &EmergencyContactDialog::onCancelClicked);
 }
@@ -19,7 +21,19 @@ void EmergencyContactDialog::onOKClicked()
     m_Contact.relation= ui->relationLineEdit->text().toStdString();
     m_Contact.address= ui->addressLineEdit->text().toStdString();
     m_Contact.contactNo= ui->contactNumLineEdit->text().toStdString();
-    accept();
+    if(!m_Contact.name.empty() && !m_Contact.relation.empty() && !m_Contact.address.empty() && !m_Contact.contactNo.empty())
+    {
+        accept();
+    }
+    else
+    {
+        QMessageBox::warning(
+            this,
+            "Add Contact Failed",
+            "Unable to save dependent.\nBlank Fields"
+        );
+        this->setResult(QDialog::Rejected);
+    }
 };
 
 void EmergencyContactDialog::onCancelClicked()
