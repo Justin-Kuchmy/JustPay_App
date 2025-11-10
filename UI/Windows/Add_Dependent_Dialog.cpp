33208373c57a1525ab89d8fe7b787d98fc624e3d
Justin Kuchmy <Jukuchmy@gmail.com>
2025-11-10 15:14:57 +0800

    //disable auto close after accepted

--- UI/Windows/Add_Dependent_Dialog.cpp
+++ UI/Windows/Add_Dependent_Dialog.cpp
@@ -1,11 +1,14 @@
 #include "include/Generated/ui_add_dependent.h"
 #include "UI/Add_Dependent_Dialog.h"
+#include "Services/AppContext.h"
 
 AddDependentDialog::AddDependentDialog(QWidget *parent): QDialog(parent), ui(new Ui::AddDependentDialog)
 {
     ui->setupUi(this); 
-    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &AddDependentDialog::onOKClicked);
+    disconnect(ui->buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept); //disable auto close after accepted
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, &AddDependentDialog::onOKClicked); //to close myself
     connect(ui->buttonBox, &QDialogButtonBox::rejected, this, &AddDependentDialog::onCancelClicked);
+
 }
 
 AddDependentDialog::~AddDependentDialog()
@@ -18,7 +21,22 @@ void AddDependentDialog::onOKClicked()
     m_Dependent.name = ui->nameLineEdit->text().toStdString();
     m_Dependent.relation = ui->relationLineEdit->text().toStdString();
     m_Dependent.birthday = from_string(ui->bdayDateEdit->date().toString("yyyy-MM-dd").toStdString());
-    accept();
+    auto today = Date::getTodayDate();
+    if (m_Dependent.birthday > today) {
+        LOG_DEBUG("[AddDependentDialog] record failed check birthday");
+        QMessageBox::warning(
+            this,
+            "Add Dependent Failed",
+            "Unable to save dependent.\nPlease check birthday"
+        );
+        this->setResult(QDialog::Rejected);
+    }
+    else
+    {
+        LOG_DEBUG("[AddDependentDialog] Dependent recorded");
+        accept();
+    }
+
 };
 
 void AddDependentDialog::onCancelClicked()
