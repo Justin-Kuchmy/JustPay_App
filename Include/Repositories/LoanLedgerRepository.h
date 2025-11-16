e32c166006280273cf6bc221ccfa68e97123c2c4
Justin Kuchmy <Jukuchmy@gmail.com>
2025-11-16 10:04:34 +0800

    implemented header

---
+++ Include/Repositories/LoanLedgerRepository.h
@@ -0,0 +1,31 @@
+#ifndef LOANLEDGEREPO_H
+#define LOANLEDGEREPO_H
+#include "BaseRepository.h"
+#include "../Models/DataObjects.h"
+#include <sqlite3.h>
+
+class LoanLedgerRepository: public BaseRepository
+{
+    static LoanLedger mapLoanLedger(sqlite3_stmt* stmt);
+    public: 
+        explicit LoanLedgerRepository(sqlite3* db);
+
+        std::string getCreateTableSQL() const override;
+
+        //create
+        int insertLoanLedger(const LoanLedger& loanLedger);
+        
+        //read
+        std::optional<LoanLedger> getById(int id);
+        std::vector<LoanLedger> getAllById(std::string id);
+        
+        //update
+        bool updateLoanLedger(const LoanLedger& lled);
+        
+        //delete
+        bool deleteLoanLedger(int id);
+        std::string getLastLoanLedgerId();
+};
+
+
+#endif
