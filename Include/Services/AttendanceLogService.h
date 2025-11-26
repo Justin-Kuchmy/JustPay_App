e6e46c32503f43c326d90e71520ec2471e9aa5ab
Justin Kuchmy <Jukuchmy@gmail.com>
2025-11-26 19:32:34 +0800

    newly created

---
+++ Include/Services/AttendanceLogService.h
@@ -0,0 +1,28 @@
+#ifndef ATTENDANCELOGSERVICE_H
+#define ATTENDANCELOGSERVICE_H
+#include "Repositories/AttendanceLogRepository.h"
+
+class AttendanceLogService
+{
+    protected:
+        AttendanceLogRepository& repo;
+    public:
+        explicit AttendanceLogService(AttendanceLogRepository& r);
+
+        //CREATE
+        int addAttendanceLog(const AttendanceLog& AttendanceLog);
+
+        //READ
+        std::optional<AttendanceLog> getAttendanceLogByID(int id);
+
+        std::vector<AttendanceLog> getAllAttendanceLogs();
+
+        //UPDATE
+        bool updateAttendanceLog(const AttendanceLog& e);
+        
+        //DELETE
+        bool deleteAttendanceLog(int id);
+ 
+};
+
+#endif
