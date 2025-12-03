480e4218222311b8279487b20766c4ff6fcfca87
Justin Kuchmy <Jukuchmy@gmail.com>
2025-12-03 18:01:53 +0800

    added declarationsq

--- Include/UI/Attendance_List_Widget.h
+++ Include/UI/Attendance_List_Widget.h
@@ -2,24 +2,35 @@
 #define ATTENDANCELISTWIDGET_H
 
 #include <QWidget>
+#include <vector>
+#include <QSortFilterProxyModel>
+#include "Models/DataObjects.h"
+#include "Models/QObjects.h"
+#include "Utils/BaseContentWidget.h"
 
 namespace Ui
 {
     class AttendanceListWidget;
 }
 
-class AttendanceListWidget: public QWidget
-{
+class AttendanceListWidget: public BaseContentWidget {
     Q_OBJECT
 public:
     AttendanceListWidget(QWidget *parent = nullptr);
     ~AttendanceListWidget();
+
 private slots:
-    void onReloadClicked();
+    void onEmployeeSearchBoxChanged(const QString& text);
+    void onReloadLogs();
     void onAddClicked();
+    void onRowClicked(const QModelIndex &index);
 
 private:
     Ui::AttendanceListWidget* ui;
+    std::vector<AttendanceLog> attendanceLogs;
+    AttendanceLogModel* model;
+    QSortFilterProxyModel* proxyModel;
+
 };
 
 #endif
