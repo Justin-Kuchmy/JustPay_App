024ac6d6de548af0c4eb440bf80e2dcb46b0b997
Justin Kuchmy <Jukuchmy@gmail.com>
2025-12-03 18:02:35 +0800

    added attendancelog context

--- Include/UI/Employee_Details_Widget.h
+++ Include/UI/Employee_Details_Widget.h
@@ -13,6 +13,7 @@
 #include "UI/Edit_Dependent_Details.h"
 #include "UI/Edit_Emergency_Contact_Details.h"
 #include "UI/Loan_Ledger_Widget.h"
+#include "UI/Employee_Attendance_Widget.h"
 #include "Repositories/EmployeeRepository.h"
 #include "Services/AppContext.h"
 #include "Utils/Log.h"
@@ -41,11 +42,13 @@ private:
     EditEmergencyContactDetails* editContactForm;
     EditDependentDetails* editDependentForm;
     LoanLedgerWidget* loanLedgetWidget;
+    EmployeeAttendanceWidget* employeeAttendanceLogWidget;
     void refreshEmployeeList(const QString &filter = QString());
     bool loadEmployeeDetails(QString& empID);
     std::optional<Contact> optContact;
     std::optional<Dependent> optDependent;
     std::vector<LoanLedger> optLoanLedgers;
+    std::vector<AttendanceLog> optAttendanceLogs;
     
 };
 
