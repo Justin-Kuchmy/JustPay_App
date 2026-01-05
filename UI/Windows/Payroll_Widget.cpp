71f28a56774d29a2ff17ae1631687869fe9c81e5
Justin Kuchmy <Jukuchmy@gmail.com>
2026-01-05 16:07:29 +0800

    next & prev window methods updated

--- UI/Windows/Payroll_Widget.cpp
+++ UI/Windows/Payroll_Widget.cpp
@@ -20,6 +20,9 @@ payrollResults{}, employeeValidation{}, premiumValue{}, results{}
     connect(employeeValidation, &EmployeeValidationWidget::dataLoaded, this, [this]() {
         ui->nextButton->setEnabled(true);
     });
+    connect(premiumValue, &PremiumValuesWidget::valuesApplied, this, [this]() {
+        ui->nextButton->setEnabled(true);
+    });
     
 
 }
@@ -35,19 +38,44 @@ PayrollWidget::~PayrollWidget()
 void PayrollWidget::nextWindow()
 {
     QStackedWidget* stack = ui->stackedWidget;
-    int index = stack->currentIndex();
-
-    if (index <= stack->count() - 1) {
-        stack->setCurrentIndex(index + 1);
+    
+    if (pageNumber <= stack->count() - 1) {
+        pageNumber = stack->currentIndex()+1;
+        stack->setCurrentIndex(pageNumber);
+    }
+    switch(pageNumber)
+    {
+        case 1:
+            ui->nextButton->setEnabled(false);
+            break;
+        case 2: 
+            results->loadTableData();
+            break;
+        default:
+            break;
     }
 }
+
 void PayrollWidget::previousWindow()
 {
     QStackedWidget* stack = ui->stackedWidget;
-    int index = stack->currentIndex();
+    
+    if (pageNumber >= 0) {
+        pageNumber = stack->currentIndex()-1;
+        stack->setCurrentIndex(pageNumber);
+    }
+    switch(pageNumber)
+    {
+        case 0:
+            ui->nextButton->setEnabled(false);
+            LOG_DEBUG("Back: employeeValidation Page: " << stack->currentIndex());
+            break;
+        case 1:
 
-    if (index >= 0) {
-        stack->setCurrentIndex(index - 1);
+            LOG_DEBUG("Back: Premium Values Page: " << stack->currentIndex());
+            break;
+        default:
+            break;
     }
 }
 
