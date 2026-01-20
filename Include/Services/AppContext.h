ccce4e55f4b751719490425589adaf4b29c81e79
Justin Kuchmy <JuKuchmy@gmail.com>
2026-01-20 17:26:12 +0800

    added payroll

--- Include/Services/AppContext.h
+++ Include/Services/AppContext.h
@@ -6,11 +6,13 @@
 #include "Services/EmergencyContactService.h"
 #include "Services/LoanLedgerService.h"
 #include "Services/AttendanceLogService.h"
+#include "Services/PayrollService.h"
 #include "Repositories/EmployeeRepository.h"
 #include "Repositories/DependentRepository.h"
 #include "Repositories/EmergencyContactRepository.h"
 #include "Repositories/LoanLedgerRepository.h"
 #include "Repositories/AttendanceLogRepository.h"
+#include "Repositories/PayrollRepository.h"
 #include <sqlite3.h>
 
 class EmployeeRepository;
@@ -18,43 +20,47 @@ class DependentRepository;
 class EmergencyContactRepository;
 class LoanLedgerRepository;
 class AttendanceLogRepository;
+class PayrollRepository;
 
 class EmployeeService;
 class DependentService;
 class EmergencyContactService;
 class LoanLedgerService;
 class AttendanceLogService;
+class PayrollService;
 
-class AppContext 
+class AppContext
 {
 public:
-    static AppContext& instance(const std::string& dbName = "payroll.db");
-  
+    static AppContext &instance(const std::string &dbName = "payroll.db");
 
-    EmployeeService& employeeService() noexcept;
-    DependentService& dependentService() noexcept;
-    EmergencyContactService& emergencyContactService() noexcept;
-    LoanLedgerService& loanLedgerService() noexcept;
-    AttendanceLogService& attendanceLogService() noexcept;
-private: 
-    sqlite3* m_db = nullptr;
+    EmployeeService &employeeService() noexcept;
+    DependentService &dependentService() noexcept;
+    EmergencyContactService &emergencyContactService() noexcept;
+    LoanLedgerService &loanLedgerService() noexcept;
+    AttendanceLogService &attendanceLogService() noexcept;
+    PayrollService &payrollService() noexcept;
+
+private:
+    sqlite3 *m_db = nullptr;
     EmployeeRepository m_employeeRepo;
     DependentRepository m_dependentRepo;
     EmergencyContactRepository m_emergencyContactRepo;
     LoanLedgerRepository m_loanLedgerRepo;
     AttendanceLogRepository m_attendanceLogRepo;
+    PayrollRepository m_payrollRepo;
 
     EmployeeService m_employeeService;
     DependentService m_dependentService;
     EmergencyContactService m_emergencyContactService;
     LoanLedgerService m_loanLedgerService;
     AttendanceLogService m_attendanceLogService;
+    PayrollService m_payrollService;
 
-
-    explicit AppContext(const std::string& dbName);
+    explicit AppContext(const std::string &dbName);
     ~AppContext() noexcept;
-    
-    AppContext(const AppContext&) = delete; 
-    AppContext& operator=(const AppContext&) = delete;
+
+    AppContext(const AppContext &) = delete;
+    AppContext &operator=(const AppContext &) = delete;
 };
 #endif
--- Source/Services/AppContext.cpp
+++ Source/Services/AppContext.cpp
@@ -3,12 +3,13 @@
 #include <QApplication>
 #include <QDir>
 
-sqlite3* openDb(const std::string& dbName) {
-    sqlite3* db = nullptr;
+sqlite3 *openDb(const std::string &dbName)
+{
+    sqlite3 *db = nullptr;
     QString exeDir = QCoreApplication::applicationDirPath();
     QString dbPath = QDir(exeDir).filePath("../Resources/" + QString::fromStdString(dbName));
     std::string dbPathStr = dbPath.toStdString();
-    const char* cpath = dbPathStr.c_str();
+    const char *cpath = dbPathStr.c_str();
 
     if (sqlite3_open(cpath, &db) != SQLITE_OK)
         throw std::runtime_error("Failed to open database");
@@ -16,66 +17,71 @@ sqlite3* openDb(const std::string& dbName) {
     return db;
 }
 
-AppContext& AppContext::instance(const std::string& dbName) 
+AppContext &AppContext::instance(const std::string &dbName)
 {
     static AppContext ctx(dbName);
     return ctx;
 }
 
-AppContext::AppContext(const std::string& dbName):
-m_db(openDb(dbName)),
-m_employeeRepo(m_db),
-m_dependentRepo(m_db),
-m_emergencyContactRepo(m_db),
-m_loanLedgerRepo(m_db),
-m_attendanceLogRepo(m_db),
-m_employeeService(m_employeeRepo),
-m_dependentService(m_dependentRepo),
-m_emergencyContactService(m_emergencyContactRepo),
-m_loanLedgerService(m_loanLedgerRepo),
-m_attendanceLogService(m_attendanceLogRepo)
+AppContext::AppContext(const std::string &dbName) : m_db(openDb(dbName)),
+                                                    m_employeeRepo(m_db),
+                                                    m_dependentRepo(m_db),
+                                                    m_emergencyContactRepo(m_db),
+                                                    m_loanLedgerRepo(m_db),
+                                                    m_attendanceLogRepo(m_db),
+                                                    m_payrollRepo(m_db),
+                                                    m_employeeService(m_employeeRepo),
+                                                    m_dependentService(m_dependentRepo),
+                                                    m_emergencyContactService(m_emergencyContactRepo),
+                                                    m_loanLedgerService(m_loanLedgerRepo),
+                                                    m_attendanceLogService(m_attendanceLogRepo),
+                                                    m_payrollService(m_payrollRepo)
 {
-   
+
     m_employeeRepo.createTable();
     m_dependentRepo.createTable();
     m_emergencyContactRepo.createTable();
     m_loanLedgerRepo.createTable();
     m_attendanceLogRepo.createTable();
+    m_payrollRepo.createTable();
 };
 
 AppContext::~AppContext()
 {
     std::cout << "\nAppContext destroyed!";
-    if (m_db) {
+    if (m_db)
+    {
         sqlite3_close(m_db);
         std::cout << "\nm_db closed";
     }
-
 };
 
-
-
-EmployeeService& AppContext::employeeService() noexcept
+EmployeeService &AppContext::employeeService() noexcept
 {
     return m_employeeService;
 };
 
-DependentService& AppContext::dependentService() noexcept
+DependentService &AppContext::dependentService() noexcept
 {
     return m_dependentService;
 };
 
-EmergencyContactService& AppContext::emergencyContactService() noexcept
+EmergencyContactService &AppContext::emergencyContactService() noexcept
 {
     return m_emergencyContactService;
 };
 
-LoanLedgerService& AppContext::loanLedgerService() noexcept
+LoanLedgerService &AppContext::loanLedgerService() noexcept
 {
     return m_loanLedgerService;
 };
 
-AttendanceLogService& AppContext::attendanceLogService() noexcept
+AttendanceLogService &AppContext::attendanceLogService() noexcept
 {
     return m_attendanceLogService;
+}
+
+PayrollService &AppContext::payrollService() noexcept
+{
+    return m_payrollService;
 }
