d31375252ef0492c52efe725a33a57f473cd3db3
Justin Kuchmy <Jukuchmy@gmail.com>
2026-02-04 18:02:27 +0800

    added ref to AuthService

--- UI/Windows/Email_Login_Widget.cpp
+++ UI/Windows/Email_Login_Widget.cpp
@@ -1,12 +1,16 @@
 #include "include/Generated/ui_email_login_widget.h"
 #include "UI/Email_Login_Widget.h"
 
-EmailLoginWidget::EmailLoginWidget(QWidget *parent) : QWidget(parent), ui(new Ui::EmailLoginWidget)
+EmailLoginWidget::EmailLoginWidget(QWidget *parent) : BaseContentWidget(parent), ui(new Ui::EmailLoginWidget)
 {
     ui->setupUi(this);
     ui->appPasswordEdit->setEchoMode(QLineEdit::Password);
-    connect(ui->loginButton, &QPushButton::clicked, this, &EmailLoginWidget::onLoginButtonClicked);
+    connect(ui->saveButton, &QPushButton::clicked, this, &EmailLoginWidget::onSaveButtonClicked);
     connect(ui->passCheckBox, &QCheckBox::clicked, this, &EmailLoginWidget::onPassCheckBoxClicked);
+    connect(ui->backButton, &QPushButton::clicked, this, &BaseContentWidget::backRequested);
+    emailCrudentials = AuthService::getInstance().createOrReadSettingsFile();
+    ui->emailLineEdit->setText(QString::fromStdString(emailCrudentials.companyEmail));
+    ui->appPasswordEdit->setText(QString::fromStdString(emailCrudentials.appPassword));
 }
 
 EmailLoginWidget::~EmailLoginWidget()
@@ -14,9 +18,12 @@ EmailLoginWidget::~EmailLoginWidget()
     delete ui;
 }
 
-void EmailLoginWidget::onLoginButtonClicked()
+void EmailLoginWidget::onSaveButtonClicked()
 {
-    qDebug() << "Login Button Clicked";
+
+    emailCrudentials.companyEmail = ui->emailLineEdit->text().toStdString();
+    emailCrudentials.appPassword = ui->appPasswordEdit->text().toStdString();
+    AuthService::getInstance().writeJsonFile(emailCrudentials);
 }
 
 void EmailLoginWidget::onPassCheckBoxClicked()
