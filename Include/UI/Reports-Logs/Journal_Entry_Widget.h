8de0d5432fb38baf0a5be250f9b39a44c4e3659e
Justin Kuchmy <Jukuchmy@gmail.com>
2026-03-04 11:43:49 +0800

    added declarations

--- Include/UI/Reports-Logs/Journal_Entry_Widget.h
+++ Include/UI/Reports-Logs/Journal_Entry_Widget.h
@@ -2,6 +2,7 @@
 #define JOURNALENTRYWIDGET_H
 #include <QWidget>
 #include "Utils/BaseContentWidget.h"
+#include <QLocale>
 namespace Ui
 {
     class JournalEntryWidget;
@@ -12,8 +13,20 @@ class JournalEntryWidget : public BaseContentWidget
 public:
     JournalEntryWidget(QWidget *parent = nullptr);
     ~JournalEntryWidget();
+    JournalEntryWidget(const JournalEntryWidget &) = delete;
+    JournalEntryWidget &operator=(const JournalEntryWidget &) = delete;
+    void onPayrollPeriodChanged();
 
 private:
     Ui::JournalEntryWidget *ui;
+    QStringList entryPayPeriods;
+    std::vector<JournalEntry> entries;
+    std::vector<JournalEntry> *entriesPtr = nullptr;
+    double debitTotals{};
+    double creditTotals{};
+
+    JournalEntryModel *model;
+    JournalEntryFilterProxyModel *m_proxy;
+    QLocale phLocale{QLocale::English, QLocale::Philippines};
 };
 #endif
