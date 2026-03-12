cff8001b931a97f1b066b21b76a460ef582dbf01
Justin Kuchmy <Jukuchmy@gmail.com>
2026-03-12 19:42:26 +0800

    implementation

--- Include/Services/GovernmentRemittanceService.h
+++ Include/Services/GovernmentRemittanceService.h
@@ -3,83 +3,77 @@
 #include "Repositories/GovernmentRemittanceRepository.h"
 #include "Repositories/PayrollRepository.h"
 
-// Get status summary for a period
-struct PeriodStatusSummary
-{
-    std::string payPeriodText;
-    int payPeriodHalf;
-    int sssRemittancesPending{0};
-    int sssRemittancesSubmitted{0};
-    int sssRemittancesConfirmed{0};
-    int phicRemittancesPending{0};
-    int phicRemittancesSubmitted{0};
-    int phicRemittancesConfirmed{0};
-    int hdmfRemittancesPending{0};
-    int hdmfRemittancesSubmitted{0};
-    int hdmfRemittancesConfirmed{0};
-};
-
-struct PeriodTotals
-{
-    std::string payPeriodText;
-    int payPeriodHalf;
-    int employeeCount{0};
-    double totalSSSEE{0.0};
-    double totalSSSER{0.0};
-    double totalPHICEE{0.0};
-    double totalPHICER{0.0};
-    double totalHDMFEE{0.0};
-    double totalHDMFER{0.0};
-    double totalWithholdingTax{0.0};
-
-    double totalEmployerCost() const
-    {
-        return totalSSSER + totalPHICER + totalHDMFER;
-    }
-
-    double totalEmployeeCost() const
-    {
-        return totalSSSEE + totalPHICEE + totalHDMFEE;
-    }
-
-    double grandTotal() const
-    {
-        return totalEmployerCost() + totalEmployeeCost() + totalWithholdingTax;
-    }
-};
-
 class GovernmentRemittanceService
 {
-protected:
+private:
     GovernmentRemittanceRepository &remittanceRepo;
     PayrollRepository &payrollRepo;
 
 public:
+    struct PeriodStatusSummary
+    {
+        std::string payPeriodText;
+        int payPeriodHalf;
+        int sssRemittancesPending{0};
+        int sssRemittancesSubmitted{0};
+        int sssRemittancesConfirmed{0};
+        int phicRemittancesPending{0};
+        int phicRemittancesSubmitted{0};
+        int phicRemittancesConfirmed{0};
+        int hdmfRemittancesPending{0};
+        int hdmfRemittancesSubmitted{0};
+        int hdmfRemittancesConfirmed{0};
+    };
+
+    struct PeriodTotals
+    {
+        std::string payPeriodText;
+        int payPeriodHalf;
+        int employeeCount{0};
+        double totalSSSEE{0.0};
+        double totalSSSER{0.0};
+        double totalPHICEE{0.0};
+        double totalPHICER{0.0};
+        double totalHDMFEE{0.0};
+        double totalHDMFER{0.0};
+        double totalWithholdingTax{0.0};
+
+        double totalEmployerCost() const
+        {
+            return totalSSSER + totalPHICER + totalHDMFER;
+        }
+
+        double totalEmployeeCost() const
+        {
+            return totalSSSEE + totalPHICEE + totalHDMFEE;
+        }
+
+        double grandTotal() const
+        {
+            return totalEmployerCost() + totalEmployeeCost() + totalWithholdingTax;
+        }
+    };
     explicit GovernmentRemittanceService(GovernmentRemittanceRepository &remRepo, PayrollRepository &payRepo);
-
-    std::vector<GovernmentRemittance> getRemittancesForWidget(const std::string &payPeriodText, int payPeriodHalf);
-
-    bool generateRemittanceFromPayrolls(const std::vector<PayrollCalculationResults> *payrolls);
-
-    std::vector<GovernmentRemittance> generateRemittancesForPeriod(const std::string &payPeriodText, int payPeriodHalf);
-
-    bool submitRemittancesForPeriod(const std::string &payPeriodText, int payPeriodHalf, const std::string &remittanceType, int id);
-
+    sqlite3_int64 addRemittance(const GovernmentRemittance &r);
+    std::vector<sqlite3_int64> addRemittanceReports(const std::vector<GovernmentRemittance> &r);
+    std::vector<GovernmentRemittance> generateFromPayroll(const std::vector<PayrollCalculationResults> *payrolls);
+    std::optional<GovernmentRemittance> getById(int id);
+    std::optional<GovernmentRemittance> getByPayrollId(int payrollId);
+    std::vector<GovernmentRemittance> getByPeriod(const std::string &payPeriodText, std::optional<int> payPeriodHalf = std::nullopt);
+    std::vector<GovernmentRemittance> getByEmployee(const std::string &employeeId);
+    std::vector<GovernmentRemittance> getPending();
+    std::vector<GovernmentRemittance> getAll();
     GovernmentRemittanceRepository::MonthlySummary getMonthlySummary(const std::string &monthYear);
-
-    PeriodTotals calculatePeriodTotals(const std::string &payPeriodText, int payPeriodHalf);
-
-    bool submitRemittance(int remittanceId, const std::string &remittanceType, int submittedByUserId);
-
-    bool confirmRemittance(int remittanceId, const std::string &remittanceType);
-
-    bool rejectRemittance(int remittanceId, const std::string &remittanceType);
-
-    bool isPeriodFullySubmitted(const std::string &payPeriodText, int payPeriodHalf, const std::string &remittanceType);
-
-    bool isPeriodFullyConfirmed(const std::string &payPeriodText, int payPeriodHalf, const std::string &remittanceType);
-
     PeriodStatusSummary getPeriodStatusSummary(const std::string &payPeriodText, int payPeriodHalf);
+    PeriodTotals getPeriodTotals(const std::string &payPeriodText, int payPeriodHalf);
+    double getTotalEmployerContribution(const std::vector<GovernmentRemittance> &remittances);
+    double getTotalEmployeeContribution(const std::vector<GovernmentRemittance> &remittances);
+    double getTotalRemittance(const std::vector<GovernmentRemittance> &remittances);
+    bool submit(int remittanceId, const std::string &remittanceType, int submittedByUserId, const Date &submissionDate);
+    bool confirm(int remittanceId, const std::string &remittanceType);
+    bool reject(int remittanceId, const std::string &remittanceType);
+    bool update(const GovernmentRemittance &remittance);
+    bool remove(int id);
 };
 
 #endif
