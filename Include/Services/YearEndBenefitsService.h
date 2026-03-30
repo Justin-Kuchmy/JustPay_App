42021ac88103b8056faff3ef97074e542c63ba1b
Justin Kuchmy <Jukuchmy@gmail.com>
2026-03-30 14:11:53 +0800

    added service and repos context

--- Include/Services/YearEndBenefitsService.h
+++ Include/Services/YearEndBenefitsService.h
@@ -1,16 +1,18 @@
 #ifndef YEARENDBENEFITSERVICE_H
 #define YEARENDBENEFITSERVICE_H
 #include "Repositories/PayrollRepository.h"
-#include "Repositories/LeaveRepository.h"
+#include "Repositories/EmployeeRepository.h"
+#include "Services/LeaveBalanceService.h"
 
 class YearEndBenefitsService
 {
 protected:
     PayrollRepository &payrollRepo;
-    LeaveRepository &leaveRepo;
+    LeaveBalanceService &leaveBalanceService;
+    EmployeeRepository &empRepo;
 
 public:
-    explicit YearEndBenefitsService(PayrollRepository &payrollRepo, LeaveRepository &leaveRepo);
+    explicit YearEndBenefitsService(PayrollRepository &payrollRepo, LeaveBalanceService &leaveBalanceService, EmployeeRepository &empRepo);
     ~YearEndBenefitsService();
     YearEndBenefits compute(std::string employeeId, int year);
 };
