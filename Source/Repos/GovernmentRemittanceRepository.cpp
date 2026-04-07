5804a01a6dd2ef63ba823fe2c544878319ec388d
Justin Kuchmy <Jukuchmy@gmail.com>
2026-04-07 15:55:03 +0800

    overhaul on columns

--- Source/Repos/GovernmentRemittanceRepository.cpp
+++ Source/Repos/GovernmentRemittanceRepository.cpp
@@ -17,18 +17,12 @@ GovernmentRemittance GovernmentRemittanceRepository::mapRemittance(sqlite3_stmt 
     gr.employeeDepartment = reinterpret_cast<const char *>(sqlite3_column_text(stmt, idx++));
     gr.payPeriodText = reinterpret_cast<const char *>(sqlite3_column_text(stmt, idx++));
     gr.payPeriodHalf = sqlite3_column_int(stmt, idx++);
-    gr.sssPremium_EE = sqlite3_column_double(stmt, idx++);
-    gr.sssPremium_ER = sqlite3_column_double(stmt, idx++);
-    gr.sssPremiumTotal = sqlite3_column_double(stmt, idx++);
-    gr.sssSubmissionStatus = static_cast<RemittanceStatus>(sqlite3_column_int(stmt, idx++));
-    gr.philHealthPremium_EE = sqlite3_column_double(stmt, idx++);
-    gr.philHealthPremium_ER = sqlite3_column_double(stmt, idx++);
-    gr.philHealthPremiumTotal = sqlite3_column_double(stmt, idx++);
-    gr.phicSubmissionStatus = static_cast<RemittanceStatus>(sqlite3_column_int(stmt, idx++));
-    gr.hdmfPremium_EE = sqlite3_column_double(stmt, idx++);
-    gr.hdmfPremium_ER = sqlite3_column_double(stmt, idx++);
-    gr.hdmfPremiumTotal = sqlite3_column_double(stmt, idx++);
-    gr.hdmfSubmissionStatus = static_cast<RemittanceStatus>(sqlite3_column_int(stmt, idx++));
+    gr.employee_Contrib = sqlite3_column_double(stmt, idx++);
+    gr.employer_Contrib = sqlite3_column_double(stmt, idx++);
+    gr.total_Contrib = sqlite3_column_double(stmt, idx++);
+    gr.contrib_Type = static_cast<RemittanceType>(sqlite3_column_int(stmt, idx++));
+    gr.submissionStatus = static_cast<RemittanceStatus>(sqlite3_column_int(stmt, idx++));
+
     gr.withHoldingTax = sqlite3_column_double(stmt, idx++);
     gr.withHoldingTaxSubmissionStatus = static_cast<RemittanceStatus>(sqlite3_column_int(stmt, idx++));
     gr.lastSubmittedDate = Date::fromString(reinterpret_cast<const char *>(sqlite3_column_text(stmt, idx++)));
@@ -53,25 +47,18 @@ sqlite3_int64 GovernmentRemittanceRepository::insertRemittance(const GovernmentR
             employee_department,
             pay_period_text,
             pay_period_half,
-            sss_premium_ee,
-            sss_premium_er,
-            sss_premium_total,
-            sss_submission_status,
-            phic_premium_ee,
-            phic_premium_er,
-            phic_premium_total,
-            phic_submission_status,
-            hdmf_premium_ee,
-            hdmf_premium_er,
-            hdmf_premium_total,
-            hdmf_submission_status,
+            employee_Contrib,
+            employer_Contrib,
+            total_Contrib,
+            remittance_type,
+            submission_status,
             withholding_tax,
             withholding_tax_submission_status,
             last_submitted_date,
             submitted_by_user_id,
             date_created,
             date_modified
-        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
     )";
     if (execute(sql, [&r](sqlite3_stmt *stmt)
                 { bindRemittance(stmt, r); }))
@@ -136,97 +123,33 @@ std::vector<GovernmentRemittance> GovernmentRemittanceRepository::getRemittances
 };
 std::vector<GovernmentRemittance> GovernmentRemittanceRepository::getRemittancesByStatus(RemittanceStatus status)
 {
-    std::string sql = std::format("select * from government_remittances WHERE sss_submission_status = {} OR phic_submission_status = {} OR hdmf_submission_status = {} OR withholding_tax_submission_status = {};", static_cast<int>(status), static_cast<int>(status), static_cast<int>(status), static_cast<int>(status));
-
-    auto results = BaseRepository::query<GovernmentRemittance>(sql, mapRemittance);
-    if (results.size() > 0)
-    {
-        return results;
-    }
-    else
-        return {};
+    const char *sql = "select * from government_remittances where submission_status = ?";
+    return query<GovernmentRemittance>(sql, mapRemittance, [status](sqlite3_stmt *stmt)
+                                       { sqlite3_bind_int(stmt, 1, static_cast<int>(status)); });
 };
 
 std::vector<GovernmentRemittance> GovernmentRemittanceRepository::getRemittancesByPeriod(const std::string &payPeriodText, std::optional<int> payPeriodHalf)
 {
-    std::string sql = "SELECT * FROM government_remittances WHERE pay_period_text = ?";
+
+    std::string sql = "select * from government_remittances where pay_period_text = ?";
     if (payPeriodHalf.has_value())
         sql += " AND pay_period_half = ?";
-
-    sqlite3_stmt *stmt = nullptr;
-    std::vector<GovernmentRemittance> results{};
-
-    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK)
-    {
-        LOG_DEBUG("Failed to prepare: " << sqlite3_errmsg(db));
-        return results;
-    }
-    int idx = 1;
-    sqlite3_bind_text(stmt, idx++, payPeriodText.c_str(), -1, SQLITE_TRANSIENT);
-
-    if (payPeriodHalf.has_value())
-    {
-        sqlite3_bind_int(stmt, idx++, payPeriodHalf.value());
-    }
-
-    while (sqlite3_step(stmt) == SQLITE_ROW)
-    {
-        auto obj = mapRemittance(stmt);
-        results.push_back(obj);
-    }
-
-    sqlite3_finalize(stmt);
-    return results;
+    return query<GovernmentRemittance>(sql, mapRemittance, [payPeriodText, payPeriodHalf](sqlite3_stmt *stmt)
+                                       { 
+                                        sqlite3_bind_text(stmt, 1, payPeriodText.c_str(), -1, SQLITE_TRANSIENT);
+                                        if (payPeriodHalf.has_value()) sqlite3_bind_int(stmt, 2, payPeriodHalf.value()); });
 }
 
 std::vector<GovernmentRemittance> GovernmentRemittanceRepository::getAllRemittances()
 {
-    std::string sql = std::format("select * from government_remittances");
-    auto results = BaseRepository::query<GovernmentRemittance>(sql, mapRemittance);
-    if (results.size() > 0)
-    {
-        return results;
-    }
-    else
-        return {};
+    const char *sql = "select * from government_remittances";
+    return query<GovernmentRemittance>(sql, mapRemittance);
 }
-GovernmentRemittanceRepository::MonthlySummary GovernmentRemittanceRepository::getMonthlySummary(const std::string &monthYear)
-{
-    // get all then loop through and add to the monthly summary object, then return the object
-    auto items = GovernmentRemittanceRepository::getRemittancesByPeriod(monthYear);
-    GovernmentRemittanceRepository::MonthlySummary summary{};
-    summary.monthYear = monthYear;
-    for (GovernmentRemittance &item : items)
-    {
-        summary.totalSSSEE += item.sssPremium_EE;
-        summary.totalSSSER += item.sssPremium_ER;
-        summary.totalPHICEE += item.philHealthPremium_EE;
-        summary.totalPHICER += item.philHealthPremium_ER;
-        summary.totalHDMFEE += item.hdmfPremium_EE;
-        summary.totalHDMFER += item.hdmfPremium_ER;
-        summary.totalWithholdingTax += item.withHoldingTax;
-    }
-    return summary;
-};
 
 // update -
 bool GovernmentRemittanceRepository::markAsSubmitted(int remittanceId, const RemittanceType &type)
 {
-    std::string column;
-    if (type == RemittanceType::SSS)
-        column = " sss_submission_status";
-    else if (type == RemittanceType::PHIC)
-        column = " phic_submission_status";
-    else if (type == RemittanceType::HDMF)
-        column = " hdmf_submission_status";
-    else
-        return 0;
-    std::string sql =
-        "UPDATE government_remittances SET "
-        "date_modified = ?, "
-        "last_submitted_date = ?, " +
-        column +
-        " = ? WHERE id = ?";
+    const char *sql = "UPDATE government_remittances SET date_modified = ?, last_submitted_date = ?, submission_status =? WHERE id = ? ";
     return execute(sql, [&remittanceId](sqlite3_stmt *stmt)
                    { 
                     int idx = 1; 
@@ -238,20 +161,7 @@ bool GovernmentRemittanceRepository::markAsSubmitted(int remittanceId, const Rem
 
 bool GovernmentRemittanceRepository::markAsConfirmed(int remittanceId, const RemittanceType &type)
 {
-    std::string column;
-    if (type == RemittanceType::SSS)
-        column = " sss_submission_status";
-    else if (type == RemittanceType::PHIC)
-        column = " phic_submission_status";
-    else if (type == RemittanceType::HDMF)
-        column = " hdmf_submission_status";
-    else
-        return 0;
-    std::string sql =
-        "UPDATE government_remittances SET "
-        "date_modified = ?, " +
-        column +
-        " = ? WHERE id = ?";
+    const char *sql = "UPDATE government_remittances SET date_modified = ?,  submission_status =? WHERE id = ? ";
     return execute(sql, [&remittanceId](sqlite3_stmt *stmt)
                    { 
                     int idx = 1; 
@@ -262,20 +172,8 @@ bool GovernmentRemittanceRepository::markAsConfirmed(int remittanceId, const Rem
 
 bool GovernmentRemittanceRepository::markAsRejected(int remittanceId, const RemittanceType &type)
 {
-    std::string column;
-    if (type == RemittanceType::SSS)
-        column = " sss_submission_status";
-    else if (type == RemittanceType::PHIC)
-        column = " phic_submission_status";
-    else if (type == RemittanceType::HDMF)
-        column = " hdmf_submission_status";
-    else
-        return 0;
-    std::string sql =
-        "UPDATE government_remittances SET "
-        "date_modified = ?, " +
-        column +
-        " = ? WHERE id = ?";
+
+    const char *sql = "UPDATE government_remittances SET date_modified = ?, submission_status = ? WHERE id = ?";
     return execute(sql, [&remittanceId](sqlite3_stmt *stmt)
                    { 
                     int idx = 1; 
@@ -286,25 +184,18 @@ bool GovernmentRemittanceRepository::markAsRejected(int remittanceId, const Remi
 
 bool GovernmentRemittanceRepository::updateRemittance(const GovernmentRemittance &remittance)
 {
-    std::string sql = R"(update government_remittances set  
+    const char *sql = R"(update government_remittances set  
         payroll_calculation_results_id = ?,
                 employee_id =?,
                 full_name =?,
                 employee_department = ?,
                 pay_period_text = ?,
                 pay_period_half = ?,
-                sss_premium_ee = ?,
-                sss_premium_er = ?,
-                sss_premium_total = ?,
-                sss_submission_status = ?,
-                phic_premium_ee = ?,
-                phic_premium_er = ?,
-                phic_premium_total = ?,
-                phic_submission_status = ?,
-                hdmf_premium_ee = ?,
-                hdmf_premium_er = ?,
-                hdmf_premium_total = ?,
-                hdmf_submission_status = ?,
+                employee_Contrib = ?,
+                employer_Contrib = ?,
+                total_Contrib = ?,
+                remittance_type = ?,
+                submission_status = ?,
                 withholding_tax = ?,
                 withholding_tax_submission_status = ?,
                 last_submitted_date = ?,
@@ -320,11 +211,7 @@ bool GovernmentRemittanceRepository::updateRemittance(const GovernmentRemittance
 
 bool GovernmentRemittanceRepository::deleteRemittance(int id)
 {
-    std::string sql = R"(
-    delete 
-    from government_remittances 
-    where id = ?
-    )";
+    const char *sql = "delete from government_remittances where id =? ";
     return execute(sql, [&id](sqlite3_stmt *stmt)
                    { sqlite3_bind_int(stmt, 1, id); });
 }
@@ -338,18 +225,11 @@ int GovernmentRemittanceRepository::bindRemittance(sqlite3_stmt *stmt, const Gov
     sqlite3_bind_text(stmt, idx++, r.employeeDepartment.c_str(), -1, SQLITE_TRANSIENT);
     sqlite3_bind_text(stmt, idx++, r.payPeriodText.c_str(), -1, SQLITE_TRANSIENT);
     sqlite3_bind_int(stmt, idx++, r.payPeriodHalf);
-    sqlite3_bind_double(stmt, idx++, r.sssPremium_EE);
-    sqlite3_bind_double(stmt, idx++, r.sssPremium_ER);
-    sqlite3_bind_double(stmt, idx++, r.sssPremiumTotal);
-    sqlite3_bind_int(stmt, idx++, static_cast<int>(r.sssSubmissionStatus));
-    sqlite3_bind_double(stmt, idx++, r.philHealthPremium_EE);
-    sqlite3_bind_double(stmt, idx++, r.philHealthPremium_ER);
-    sqlite3_bind_double(stmt, idx++, r.philHealthPremiumTotal);
-    sqlite3_bind_int(stmt, idx++, static_cast<int>(r.phicSubmissionStatus));
-    sqlite3_bind_double(stmt, idx++, r.hdmfPremium_EE);
-    sqlite3_bind_double(stmt, idx++, r.hdmfPremium_ER);
-    sqlite3_bind_double(stmt, idx++, r.hdmfPremiumTotal);
-    sqlite3_bind_int(stmt, idx++, static_cast<int>(r.hdmfSubmissionStatus));
+    sqlite3_bind_double(stmt, idx++, r.employee_Contrib);
+    sqlite3_bind_double(stmt, idx++, r.employer_Contrib);
+    sqlite3_bind_double(stmt, idx++, r.total_Contrib);
+    sqlite3_bind_int(stmt, idx++, static_cast<int>(r.contrib_Type));
+    sqlite3_bind_int(stmt, idx++, static_cast<int>(r.submissionStatus));
     sqlite3_bind_double(stmt, idx++, r.withHoldingTax);
     sqlite3_bind_int(stmt, idx++, static_cast<int>(r.withHoldingTaxSubmissionStatus));
     sqlite3_bind_text(stmt, idx++, r.lastSubmittedDate.to_string().c_str(), -1, SQLITE_TRANSIENT);
