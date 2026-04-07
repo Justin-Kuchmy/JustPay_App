9efe0d96bff7c95bee5eeecb568ea1563753b758
Justin Kuchmy <Jukuchmy@gmail.com>
2026-04-07 15:55:03 +0800

    updated

--- UI/Windows/PayrollComputation/Premium_Values_Widget.cpp
+++ UI/Windows/PayrollComputation/Premium_Values_Widget.cpp
@@ -26,32 +26,52 @@ void PremiumValuesWidget::applyValues()
 {
     const int byWeekly = 2;
     auto truncateForCurrency = [](double v)
-    { return std::trunc(v * 100.0) / 100.0; };
+    {
+        return std::stof(std::format("{:.2f}", v));
+    };
 
     for (size_t i{}; i < dataBus->size(); i++)
     {
         auto &emp = dataBus->at(i);
-        std::vector<LoanLedger> empLoans = AppContext::instance().loanLedgerService().getAllLoanLedgers(emp.employeeId);
+        emp.dateProcessed = ui->dateProcessedEdit->date();
+        emp.payPeriodText = emp.dateProcessed.toMonthYearString();
         emp.payPeriodHalf = ui->payrollPeriodComboBox->currentIndex() == 0 ? 1 : 2;
+        std::vector<LoanLedger> empLoans = AppContext::instance().loanLedgerService().getAllLoanLedgers(emp.employeeId);
+        std::vector<AttendanceLog> aLogs = AppContext::instance().attendanceLogService().getAllAttendanceLogsById(emp.employeeId);
 
+        bool isFirstHalf = (emp.payPeriodHalf == 1);
         for (size_t j{}; j < empLoans.size(); j++)
         {
-            LOG_DEBUG(empLoans[j].deductionsPerPayroll << " " << empLoans[j].deductionFirstHalf << " " << empLoans[j].deductionSecondHalf);
+            LoanLedger currLoan = empLoans[j];
+            LOG_DEBUG(currLoan.deductionsPerPayroll << " " << currLoan.deductionFirstHalf << " " << currLoan.deductionSecondHalf);
 
-            if (emp.payPeriodHalf - 1 && empLoans[j].deductionFirstHalf)
-            {
-                emp.loanDeductionsPerPayroll += empLoans[j].deductionsPerPayroll;
-            }
+            if (isFirstHalf && currLoan.deductionFirstHalf)
+                emp.loanDeductionsPerPayroll += currLoan.deductionsPerPayroll;
 
-            if (emp.payPeriodHalf - 1 && empLoans[j].deductionSecondHalf)
-            {
-                emp.loanDeductionsPerPayroll += empLoans[j].deductionsPerPayroll;
-            }
+            if (!isFirstHalf && currLoan.deductionSecondHalf)
+                emp.loanDeductionsPerPayroll += currLoan.deductionsPerPayroll;
         }
 
         emp.monthlyBasicSalary /= byWeekly;
         emp.monthlyAllowances /= byWeekly;
         emp.grossIncome = (emp.monthlyBasicSalary) + emp.overTimePay + (emp.monthlyAllowances);
+        constexpr double MonthsPerYear = 12.0;
+        constexpr double WorkingDaysPerYear = 314.0;
+        constexpr double HoursPerDay = 8.0;
+
+        const double hourlyRate = (emp.monthlyBasicSalary * 2) * MonthsPerYear / WorkingDaysPerYear / HoursPerDay;
+        double totalOPpay{0.0};
+        for (auto &item : aLogs)
+        {
+
+            int logDatePeriodHalf = (item.logDate.day <= 15) ? 1 : 2;
+            if (logDatePeriodHalf == emp.payPeriodHalf)
+            {
+                totalOPpay += item.getRateMultiplier() * hourlyRate;
+                LOG_DEBUG(item.to_string());
+            }
+        }
+        emp.overTimePay = truncateForCurrency(totalOPpay);
 
         emp.applySSS = PayrollCalc::applies(config.sssSchedule, emp.payPeriodHalf);
         emp.sssPremium_EE = truncateForCurrency(PayrollCalc::calcSSS_EE(emp.monthlyBasicSalary, emp.applySSS));
@@ -62,18 +82,17 @@ void PremiumValuesWidget::applyValues()
         emp.applyHDMF = PayrollCalc::applies(config.hdmfSchedule, emp.payPeriodHalf);
         emp.hdmfPremium_EE = truncateForCurrency(PayrollCalc::calcHDMF_EE(emp.monthlyBasicSalary, emp.applyHDMF));
 
-        emp.withHoldingTax = truncateForCurrency(PayrollCalc::calcWithholding(PayrollCalc::calcTaxableIncome(emp)));
+        double tax = PayrollCalc::calcWithholding(PayrollCalc::calcTaxableIncome(emp));
+        emp.withHoldingTax = truncateForCurrency(tax);
 
-        emp.totalDeductions = emp.sssPremium_EE + emp.philHealthPremium_EE + emp.hdmfPremium_EE + emp.withHoldingTax + emp.loanDeductionsPerPayroll;
+        emp.totalDeductions = truncateForCurrency(emp.sssPremium_EE + emp.philHealthPremium_EE + emp.hdmfPremium_EE + emp.withHoldingTax + emp.loanDeductionsPerPayroll);
 
-        emp.netPay = emp.monthlyBasicSalary + emp.overTimePay + emp.monthlyAllowances - emp.totalDeductions;
+        emp.netPay = truncateForCurrency(emp.monthlyBasicSalary + emp.overTimePay + emp.monthlyAllowances - emp.totalDeductions);
 
         emp.sssPremium_ER = truncateForCurrency(PayrollCalc::calcSSS_ER(emp.monthlyBasicSalary, emp.applySSS));
 
         emp.philHealthPremium_ER = truncateForCurrency(PayrollCalc::calcPhilHealth_ER(emp.monthlyBasicSalary, emp.applyPhilHealth));
 
         emp.hdmfPremium_ER = truncateForCurrency(PayrollCalc::calcHDMF_ER(emp.monthlyBasicSalary, emp.applyHDMF));
-
-        LOG_DEBUG(emp.to_string());
     }
 }
