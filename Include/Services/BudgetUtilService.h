62dcd0b12d6b7f910fe415e74a8d497bebb79fca
Justin Kuchmy <Jukuchmy@gmail.com>
2026-04-27 23:12:06 +0800

    Budget Utilization Service header added

---
+++ Include/Services/BudgetUtilService.h
@@ -0,0 +1,17 @@
+#ifndef BUDGETUTILSERVICE_H
+#define BUDGETUTILSERVICE_H
+#include "Services/PayrollService.h"
+#include "Services/GovernmentRemittanceService.h"
+#include "Models/reporting.h"
+
+class BudgetUtilService
+{
+protected:
+    PayrollService &pService;
+    GovernmentRemittanceService &gService;
+
+public:
+    explicit BudgetUtilService(PayrollService &pService, GovernmentRemittanceService &gService);
+    std::vector<MonthlyBudgetUtilizationReport> getMonthlyBudget();
+};
+#endif
