3d80012195577f93bbb1a80e2c2dfb3adc997ab0
Justin Kuchmy <Jukuchmy@gmail.com>
2026-05-14 16:08:03 +0800

    updated

--- Include/UI/AttendanceTracking/Add_Overtime_Hours_Dialog.h
+++ Include/UI/AttendanceTracking/Add_Overtime_Hours_Dialog.h
@@ -1,29 +1,31 @@
-#ifndef  ADDOVERTIMEHOURSDIALOG_H
-#define  ADDOVERTIMEHOURSDIALOG_H
-
+#ifndef ADDOVERTIMEHOURSDIALOG_H
+#define ADDOVERTIMEHOURSDIALOG_H
 
 #include <QDialog>
-#include <QWidget> 
+#include <QWidget>
+#include <sstream>
 
 namespace Ui
 {
     class AddOvertimeHoursDialog;
 }
 
-class AddOvertimeHoursDialog: public QDialog
+class AddOvertimeHoursDialog : public QDialog
 {
     Q_OBJECT;
+
 public:
     AddOvertimeHoursDialog(QWidget *parent = nullptr);
     ~AddOvertimeHoursDialog();
-    AddOvertimeHoursDialog(const AddOvertimeHoursDialog&) = delete; 
-    AddOvertimeHoursDialog& operator=(const AddOvertimeHoursDialog&) = delete;
+    AddOvertimeHoursDialog(const AddOvertimeHoursDialog &) = delete;
+    AddOvertimeHoursDialog &operator=(const AddOvertimeHoursDialog &) = delete;
     std::string getJsonString();
 private slots:
     void onOkayClicked();
     void onCancelClicked();
+
 private:
-    Ui::AddOvertimeHoursDialog* ui;
+    Ui::AddOvertimeHoursDialog *ui;
     std::string jsonString;
 };
 
